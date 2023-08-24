c8f0fb9fd46e5db9cebfa4c805f85a11e44a3722
Arso <arsogn991@gmail.com>
2023-08-24 10:10:06 +0100

    Les exercices effectué au debut du cours

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php include("head.php") ?>
+    <h2>Page de test</h2>
+    <p>
+        PHP.<br />
+    </p>
+    Cette page contient du code HTML avec des balises <br>
+    <?php echo "<strong>Je suis le boss du dev</strong><br>";
+        echo "J'habite à Mykonos <br>";
+        echo "J'ai 28ans <br>";
+    ?>
+
+
+
+    Voici quelques petits tests :
+    <ul>
+        <li style="color: blue;">Texte en bleu</li>
+        <li style="color: red;">Texte en rouge</li>
+        <li style="color: green;">Texte en vert</li>
+    </ul>
+    <?php
+    /* Encore du PHP
+Toujours du PHP */
+    ?>
+</body>
+
+</html>
